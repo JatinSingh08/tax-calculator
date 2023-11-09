c25a3338bcdbbcae96cd66584c1677faa2df735e
JatinSingh08 <singhjatin0812@gmail.com>
2023-11-09 15:59:22 +0530

    ⚡Feat: Added favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,20 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Layout/Navbar'
 import Body from '@/components/Layout/Body'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <Body />
-    </main>
+    <>
+      <Head>
+        <title>KoinX</title>
+      </Head>
+      <main>
+        <Navbar />
+        <Body />
+      </main>
+    </>
   )
 }
